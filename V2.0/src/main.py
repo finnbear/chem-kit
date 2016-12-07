f04e5949a7ae737c64ac072070d49a5a357341af
finnbear <finnbearone@gmail.com>
2016-12-06 19:19:03 -0800

    Added particles.

--- V2.0/src/main.py
+++ V2.0/src/main.py
@@ -6,6 +6,16 @@
 # Input #
 #########
 
+# Gassium [Ga] (A fictional gas)
+gassium_particles_initial = 5 # How many gassium particles are in the simulation initially
+gassium_rate = 0 # How many gassium particles to add or subtract each tick
+gassium_radius = 3 # The radius of a gassium particle
+
+# Brownium [Br] (A fictional particle for brownian motion)
+brownium_particles_initial = 50 # How many brownium particles are in the simulation initially
+brownium_rate = 0 # How many brownium particles to add or subtract each tick
+brownium_radius = 1 # The radius of a brownium particle
+
 ############
 # Settings #
 ############
@@ -15,10 +25,10 @@
 # Rendering
 output_directory = "../out/"
 length = 1 # Length of the video in seconds
-width = 1000 # How many pixels wide is the video
-height = 1000 # How many pixels tall is the video
+width = 500 # How many pixels wide is the video
+height = 500 # How many pixels tall is the video
 ticks_per_frame = 1 # How many simulation updates per frame of video
-frames_per_second = 30 # How many frames of video per second
+frames_per_second = 5 # How many frames of video per second
 background_color = "black" # The color of the background
 overlay_title_text = "Particle Simulator" # The title text of the overlay
 overlay_subtitle_text = "By Finn Bear" # The subtitle text of the overlay
@@ -50,6 +60,8 @@ frame_counter = 0 # How many frames have been created
 #############
 
 # Rendering
+center_x = width / 2;
+center_y = height / 2;
 max_frames = length * frames_per_second # Number of frames to render in total
 overlay_title_position = (5, 5) # Position of overlay title text
 overlay_subtitle_position = (5, 15) # Position of overlay subtitle text
@@ -71,6 +83,8 @@ def main():
 			if tick_counter % ticks_per_frame == 0:
 				draw()
 				frame_counter += 1
+		else:
+			exit(0)
 
 
 ############
@@ -79,6 +93,14 @@ def main():
 
 # Physics
 def init():
+	global center_x
+	global center_y
+	global particles
+	global gassium_particles_initial
+	global gassium_radius
+	global brownium_particles_initial
+	global brownium_radius
+
 	# Clear the output directory to remove old frames
 	for file in os.listdir(output_directory):
 		path = os.path.join(output_directory, file)
@@ -88,11 +110,48 @@ def init():
 		except Exception as e:
 		    print(e)
 
+	# Create particles
+	gassium_particles = []
+	brownium_particles = []
+	id = 0;
+	for i in range(0, gassium_particles_initial):
+		gassium_particles.append({'id': id, 'type': 'Ga', 'radius': gassium_radius, 'x': center_x, 'y': center_y, 'vx': 0, 'vy': 0})
+		id += 1
+
+	for i in range(0, brownium_particles_initial):
+		brownium_particles.append({'id': id, 'type': 'Br', 'radius': brownium_radius, 'x': center_x, 'y': center_y, 'vx': 0, 'vy': 0})
+		id += 1;
+	
+	# Add the new particles to the list of particles
+	particles.extend(gassium_particles)
+	particles.extend(brownium_particles)
+
 def tick():
-	print("Tick")
+	global particles
+
+	# A list to store the updated particles
+	newParticles = []
+
+	# Iterate throught the particles
+	for particle1 in particles:
+		newParticle = particle1
+		for particle2 in particles:
+			if particle1 != particle2:
+				newParticle = physics(newParticle, particle2)
+		newParticles.append(newParticle)
+
+	# Put the new particles back into the list of particles
+	particles = newParticles
+
+def physics(particle1, particle2):
+	#float angle = circle1.getPosition().angleToRad(circle2.getPosition());
+    #circle1.changeAcceleration(-1 * (float)Math.cos(angle), -1 * (float)Math.sin(angle));
+    #circle2.changeAcceleration((float)Math.cos(angle), (float)Math.sin(angle));
+	return particle1
 
 # Rendering
 def draw():
+	global particles
 	global frame_counter
 	global output_directory
 
@@ -103,6 +162,16 @@ def draw():
 	pixels = frame.load()
 
 	# Render
+	for particle in particles:
+		px = particle['x']
+		py = particle['y']
+		pr = particle['radius']
+		
+		for x in range(px - pr - 1, px + pr + 1):
+			for y in range(py - pr - 1, py + pr + 1):
+				if inRect(x, y, 0, 0, frame.size[0], frame.size[1]) and distance(x, y, particle['x'], particle['y']) <= particle['radius']:
+					pixels[x, y] = (255, 255, 255)
+				
 
 	# Overlays
 	overlay = ImageDraw.Draw(frame)
@@ -116,9 +185,23 @@ def draw():
 # Functions #
 #############
 
-# Physics
+# Math
 def chance(probability):
 	return random.random() < probability
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+	leftSpan = leftMax - leftMin
+	rightSpan = rightMax - rightMin
+	valueScaled = float(value - leftMin) / float(leftSpan)
+
+	return rightMin + (valueScaled * rightSpan)
+
+def distance(x1, y1, x2, y2):
+	return math.hypot(x2 - x1, y2 - y1)
+
+def inRect(x, y, bx1, by1, bx2, by2):
+	return (bx1 <= x <= bx2 and by1 <= y <= by2) or (bx1 >= x >= bx2 and by1 >= y >= by2)
+
 
 ###########
 # Program #
